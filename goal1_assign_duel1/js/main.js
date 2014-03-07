73e2884e525b37fbf31c4df7ebcb6b7e96ce05c1
Amy Harvey <AEHarvey@fullsail.edu>
2014-03-07 11:22:01 -0500

    Variable Additions
    inputting the variables for the javascript file for each player

--- goal1_assign_duel1/js/main.js
+++ goal1_assign_duel1/js/main.js
@@ -7,7 +7,19 @@
 //self-executing function
 (function(){
 
+  console.log("FIGHT!!!!")
 
-    console.log("FIGHT!!!!")
+   //players names
+    var playeroneName = "Spiderman";
+    var playertwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //players health
+    var playeroneHealth = 100;
+    var playertwoHealth = 100;
+
 
 })();
