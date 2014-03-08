42e89337b3c846ae1dcb6344901898efa02e8e34
Amy Harvey <AEHarvey@fullsail.edu>
2014-03-07 22:50:45 -0500

    Adding Var to the For
    Added Variable to the For under function for program

--- goal1_assign_duel1/js/main.js
+++ goal1_assign_duel1/js/main.js
@@ -31,6 +31,14 @@
 
         for(var i=0; i<10; i++){
             //random formula is - Math.floor(math.random() * (max - min) + min
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            console.log(f1);
+            console.log(f2);
         }
 
     };
