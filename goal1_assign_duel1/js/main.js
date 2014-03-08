27f3a1d34c082f29418902628204e19627918ece
Amy Harvey <AEHarvey@fullsail.edu>
2014-03-07 22:55:15 -0500

    inflicting damage
    putting the damage inflicted into console log

--- goal1_assign_duel1/js/main.js
+++ goal1_assign_duel1/js/main.js
@@ -37,8 +37,11 @@
             var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
             var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
 
-            console.log(f1);
-            console.log(f2);
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            
         }
 
     };
